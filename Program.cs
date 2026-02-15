7bfa56a88c7f6d5200f492e13744e3c37b201884
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-02-15 17:08:45 +0530

    create upload resume part for swager

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Http;
+using UglyToad.PdfPig;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -11,9 +14,27 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/api/resume/test", () =>
-{
-    return Results.Ok("Resume API is working 🚀");
-});
+app.MapPost("/api/resume/upload", (IFormFile file) =>
+    {
+        if (file == null || file.Length == 0)
+            return Results.BadRequest("File is required");
+
+        using var stream = file.OpenReadStream();
+        using var document = PdfDocument.Open(stream);
+
+        var text = "";
+
+        foreach (var page in document.GetPages())
+        {
+            text += page.Text;
+        }
+
+        return Results.Ok(new
+        {
+            FileName = file.FileName,
+            ExtractedTextLength = text.Length
+        });
+    })
+    .DisableAntiforgery();
 
 app.Run();
